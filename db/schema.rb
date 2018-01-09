a9e39ee3d1305d8b40981252903fc1c90d62b6b3
clancyfogarty <clancyfogarty@gmail.com>
2018-01-09 09:38:41 -0800

    Create from the command line

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822174945) do
+ActiveRecord::Schema.define(version: 20170826191244) do
 
   create_table "items", force: :cascade do |t|
-    t.string   "name"
+    t.string   "description"
     t.integer  "list_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   add_index "items", ["list_id"], name: "index_items_on_list_id"
